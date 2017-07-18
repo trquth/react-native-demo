4c80acd3f051e773496526324eb1387d03436935
mac <tranquthien@gmail.com>
2017-07-18 08:36:30 +0700

    Save code

--- demo_cross_platform/index.ios.js
+++ demo_cross_platform/index.ios.js
@@ -8,6 +8,7 @@ import {
 import App from './src/App';
 //import App from './src/3rdreactnavigation/App';
 
+
 export default class demo_cross_platform extends Component {
   store = createStore(AppReducer);
   render() {
